48bd7ca67ef26bba41ed8849656627ef5e4f6d8e
beetlerat <beetlerat@yandex.ru>
2025-05-18 17:08:33 +0300

    ME-28 Механизм обновления JWT
    Dev:
    
    - Изменен способ конфигурации `Cors`, на конфигурацию, которая поддерживает cookie с любого адреса
    - JWT разделен на несколько Token-ов. Access Token используется для определения доступа к endpoint-у. Данный Token клиент отправляет в заголовке Authorization. Время жизни данного Token задается свойством `application.jwt.token.access.expirationAfterHours`. Refresh Token используется для обновления Access Token. Время жизни Refresh Token кратно больше Access Token, оно задается в свойстве `application.jwt.token.refresh.expirationAfterDays`. Refresh Token передается внутри cookie. Когда Access Token истекает, клиент отправляет запрос на endpoint `/api/auth/refresh`. Данный endpoint из cookie достает Refresh Token и если он валиден, то возвращает клиенту новый Access Token и обновляет Refresh Token в cookie
    - Swagger модифицирован для хранения cookie

--- src/main/java/com/metaverse/files/services/auth/AuthService.java
+++ src/main/java/com/metaverse/files/services/auth/AuthService.java
@@ -3,6 +3,8 @@ package com.metaverse.files.services.auth;
 import com.metaverse.files.contexts.auth.LoginContext;
 import com.metaverse.files.contexts.auth.RegistrationContext;
 import com.metaverse.files.utils.exceptions.AuthException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 /**
  * Сервис авторизации.
@@ -16,17 +18,29 @@ public interface AuthService {
      * Авторизовать пользователя.
      *
      * @param ctx {@link LoginContext контекст авторизации}
+     * @param response HttpServletResponse, в cookies которого будет установлен refresh token
      * @return JSON Web Token
      * @throws AuthException если не удалось авторизовать пользователя
      */
-    String login(LoginContext ctx);
+    String login(LoginContext ctx, HttpServletResponse response);
 
     /**
      * Зарегистрировать пользователя.
      *
      * @param ctx {@link RegistrationContext контекст регистрации}
+     * @param response HttpServletResponse, в cookies которого будет установлен refresh token
      * @return JSON Web Token
      * @throws AuthException если не удалось зарегистрировать пользователя
      */
-    String registration(RegistrationContext ctx);
+    String registration(RegistrationContext ctx, HttpServletResponse response);
+
+    /**
+     * Обновить токен указанного запроса.
+     *
+     * @param request HttpServletRequest, в cookies которого содержится refresh JWT
+     * @param response HttpServletResponse, в cookies которого будет установлен refresh token
+     * @return обновленный JSON Web Token
+     * @throws AuthException если не удалось обновить токен
+     */
+    String refresh(HttpServletRequest request, HttpServletResponse response);
 }
